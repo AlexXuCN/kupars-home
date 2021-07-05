170ab013d262d9ec0769cbcfd0377d88f825afb2
AlexXuCN <Xu_Chengyu@outlook.com>
2021-07-05 21:18:05 +0800

    v1.0!

---
+++ init.lua
@@ -0,0 +1,15 @@
+packagename="com.alexxucn.kupars.home"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appver="1.0"
+appcode="1"
+template="tool"
+appname="Kupars's Home"
+
+--require "import" import"loadlayout" import"loadmenu" import"loadbitmap" 
